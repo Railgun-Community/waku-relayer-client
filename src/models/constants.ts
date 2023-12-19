6662ec0aaf73cf2ce18f93b85f47e6ad921e63ee
zy0n <zy0n.barto@gmail.com>
2023-12-19 15:17:27 -0500

    update constants

--- src/models/constants.ts
+++ src/models/constants.ts
@@ -1,6 +1,11 @@
 export const WAKU_RAILGUN_PUB_SUB_TOPIC = '/waku/2/railgun-relayer';
 
 export const WAKU_RAILGUN_DEFAULT_PEERS: string[] = [
-  '/dns4/waku.wecamefromapes.com/tcp/60000/p2p/16Uiu2HAmEHLjCWDhBzT1e6yLiJwSEoq9hiwvwHckJUVqpFNhXh7W',
-  '/dns4/relayer2.wecamefromapes.com/tcp/60000/p2p/16Uiu2HAkwpchmFDKiC4zwANtNYPi1TenKnG8eefEaMg4cRw4Sff1'
+  '/dns4/waku.wecamefromapes.com/tcp/8000/wss/p2p/16Uiu2HAmEHLjCWDhBzT1e6yLiJwSEoq9hiwvwHckJUVqpFNhXh7W',
+  '/dns4/waku.wecamefromapes.com/tcp/8001/wss/p2p/16Uiu2HAmLKiL2Pb2LC8TNrSMAFYCDQctawv72fVgQ3nmy5RVHN5h',
+  '/dns4/waku.wecamefromapes.com/tcp/8002/wss/p2p/16Uiu2HAm2wNgfEV8gwFxkozw4guLD1Z7rn5se1pbxGYQRUmbFeJg',
+  '/dns4/waku.wecamefromapes.com/tcp/8003/wss/p2p/16Uiu2HAmBxeUwvbqkXk4BHNEqQbNdEBYVMQvkpFwcGgpSpFKuM36',
+  '/dns4/waku.wecamefromapes.com/tcp/8004/wss/p2p/16Uiu2HAmETtQJ4QxF63KX18vqQ38TqCkfv6USK9JUJ8zMUsLJXHk',
+  '/dns4/waku.wecamefromapes.com/tcp/8005/wss/p2p/16Uiu2HAkzXcf85YMERRcxjU24dn7iHQWRBDzL6VD1Br9Q9YTiE6A',
+
 ];
