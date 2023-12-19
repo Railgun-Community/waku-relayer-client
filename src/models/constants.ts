eda2d3c66dcf25cfad615df5f8c45454d87cb7e1
zy0n <zy0n.barto@gmail.com>
2023-12-19 14:26:11 -0500

    update constants for test

--- src/models/constants.ts
+++ src/models/constants.ts
@@ -1,7 +1,6 @@
 export const WAKU_RAILGUN_PUB_SUB_TOPIC = '/waku/2/railgun-relayer';
 
 export const WAKU_RAILGUN_DEFAULT_PEERS: string[] = [
-  '/dns4/relayerv4.wecamefromapes.com/tcp/8000/wss/p2p/16Uiu2HAmCMBVq9am26T61B7FyZ6JbEDusH4c7M7AYVMwNnRuP2cg',
-  '/dns4/fleet.wecamefromapes.com/tcp/8000/wss/p2p/16Uiu2HAkyCqm8368Z1Y1pvrBcQQtb66Mjj7uvyAKGdwur3uCbqfb',
-  '/dns4/fleet.wecamefromapes.com/tcp/8001/wss/p2p/16Uiu2HAmTCCZFbQxDNF1TL9TKSPXtfF9QJtNWhCFE5M8UNzk7DQv',
+  '/dns4/waku.wecamefromapes.com/tcp/60000/p2p/16Uiu2HAmEHLjCWDhBzT1e6yLiJwSEoq9hiwvwHckJUVqpFNhXh7W',
+  '/dns4/relayer2.wecamefromapes.com/tcp/60000/p2p/16Uiu2HAkwpchmFDKiC4zwANtNYPi1TenKnG8eefEaMg4cRw4Sff1'
 ];
